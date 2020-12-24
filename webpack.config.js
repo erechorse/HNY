570f261ef164b0262cc5a5d60255454ed3f4a4f6
erechorse <935horse@gmail.com>
2020-12-24 15:12:41 +0900

    Move:docフォルダに移動

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,6 @@ module.exports = {
     // 出力するファイル名
     filename: 'bundle.js',
     // 出力先のパス（v2系以降は絶対パスを指定する必要がある）
-    path: path.join(__dirname, 'output')
+    path: path.join(__dirname, './docs/output')
   }
 };
